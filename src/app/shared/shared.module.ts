d945be565510fd9f41ee91349b53e37c71923bde
George Spasov <g.spasov@propy.com>
2017-09-26 12:12:00 +0000

    Merged in authentication (pull request #3)
    Authentication
    
    * Added signup and login components. Added Routing for the Authentication module. Added Authentication Service
    
    * Router links to login and signup
    
    * Added Shared module and Rest Client Service
    
    * Added basic http methods in the rest service. Added api url in environment variables
    
    Approved-by: Vladislav Ivanov <v.ivanov@propy.com>

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { RestClientService } from './rest-client.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [],
+  providers: [
+    RestClientService
+  ]
+})
+export class SharedModule { }
