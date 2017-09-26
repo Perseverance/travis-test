d8be7ecf9657f9b7af3b703a661921805cf74b59
George Spasov <spasov05203@gmail.com>
2017-09-26 20:08:51 +0300

    Added signup form with validation

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,23 +2,26 @@ import { SharedModule } from './../shared/shared.module';
 import { AuthenticationService } from './authentication.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { SignUpComponentComponent } from './sign-up-component/sign-up-component.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SharedModule,
-    AuthenticationRoutingModule
-  ],
-  declarations: [
-    LoginComponentComponent,
-    SignUpComponentComponent
-  ],
-  providers: [
-    AuthenticationService
-  ]
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		SharedModule,
+		AuthenticationRoutingModule
+	],
+	declarations: [
+		LoginComponentComponent,
+		SignUpComponentComponent
+	],
+	providers: [
+		AuthenticationService
+	]
 })
 export class AuthenticationModule { }
