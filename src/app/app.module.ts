185dc48f997e6867eab2a16c46437fb4ff76cb1b
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2017-10-04 12:16:20 +0300

    Added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { PropertiesModule } from './properties/properties.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AuthenticationModule } from './authentication/authentication.module';
+import {PropertiesModule} from './properties/properties.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AuthenticationModule} from './authentication/authentication.module';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AngularGoogleMapsModule } from './angular-google-maps/angular-google-maps.module';
-import { CoreModule } from './core/core.module';
-import { TranslateStore } from '@ngx-translate/core/src/translate.store';
-import { FacebookModule } from 'ngx-facebook';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AngularGoogleMapsModule} from './angular-google-maps/angular-google-maps.module';
+import {CoreModule} from './core/core.module';
+import {TranslateStore} from '@ngx-translate/core/src/translate.store';
+import {FacebookModule} from 'ngx-facebook';
+import {HomeModule} from './home/home.module';
 
 @NgModule({
 	declarations: [
@@ -20,6 +21,7 @@ import { FacebookModule } from 'ngx-facebook';
 		FormsModule,
 		ReactiveFormsModule,
 		CoreModule,
+		HomeModule,
 		AngularGoogleMapsModule,
 		AuthenticationModule,
 		PropertiesModule,
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {environment} from './../../environments/environment';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AgmCoreModule} from '@agm/core';
+import {CommonModule} from '@angular/common';
+import {CoreModule} from '../core/core.module';
+import {HomeComponent} from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+
+@NgModule({
+	declarations: [
+		HomeComponent
+	],
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		HomeRoutingModule,
+		AgmCoreModule.forRoot({
+			apiKey: environment.googleMapsApiKey,
+			libraries: ['places']
+		}),
+		CoreModule
+	],
+	providers: [],
+	bootstrap: []
+})
+export class HomeModule {
+}
