21277c66642684d3d79ad399d5603da4db9da192
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2017-10-04 15:16:52 +0300

    Google search - select first result on enter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
+import {MapsAPILoader} from '@agm/core';
+import {} from '@types/googlemaps';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	private autoComplete: any;
+	public properties: any;
+	public googleSearchForm: FormGroup;
 
-  ngOnInit() {
-  }
 
+	@ViewChild('search')
+	public searchElementRef: ElementRef;
+
+
+	constructor(private mapsAPILoader: MapsAPILoader,
+				private ngZone: NgZone,
+				private formBuilder: FormBuilder) {
+		this.googleSearchForm = this.formBuilder.group({
+			searchControl: ['', [Validators.required]]
+		});
+	}
+
+	async ngOnInit() {
+		await this.loadPlacesAutoCompleteSearch();
+	}
+
+	async loadPlacesAutoCompleteSearch() {
+		await this.mapsAPILoader.load();
+		this.autoComplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+			types: ['(regions)']
+		});
+		this.autoComplete.addListener('place_changed', () => this.placeChangedHandler(this.autoComplete));
+	}
+
+	private placeChangedHandler(autoComplete: any) {
+		this.ngZone.run(async () => {
+			// get the place result
+			const place: google.maps.places.PlaceResult = this.autoComplete.getPlace();
+			// verify result
+			if (place.geometry === undefined || place.geometry === null) {
+				const autoCompleteService = new google.maps.places.AutocompleteService();
+				autoCompleteService.getPlacePredictions({
+					'input': place.name,
+					'offset': place.name.length
+				}, (list, status) => this.firstPlacePredictionOnEnter(list, status));
+			} else {
+				console.log('Selected from dropdown');
+			}
+		});
+	}
+
+	private firstPlacePredictionOnEnter(list: any, status: any) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			const placesService = new google.maps.places.PlacesService(document.createElement('div'));
+			placesService.getDetails({
+				placeId: list[0].place_id
+			}, function detailsResult(detailsResult, placesServiceStatus) {
+				if (placesServiceStatus === google.maps.places.PlacesServiceStatus.OK) {
+					console.log(detailsResult.geometry.location.lng(), detailsResult.geometry.location.lat());
+				}
+			});
+		}
+	}
 }
