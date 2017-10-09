6ca86d841bc94015702ab29af405891a788ed5c9
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2017-10-09 17:06:44 +0300

    Footer styling

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HeaderComponent } from './header/header.component';
-import { SharedModule } from '../shared/shared.module';
-import { ErrorMessageComponent } from './error-message/error-message.component';
+import {RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HeaderComponent} from './header/header.component';
+import {SharedModule} from '../shared/shared.module';
+import {ErrorMessageComponent} from './error-message/error-message.component';
+import {FooterComponent} from './footer/footer.component';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -29,7 +30,8 @@ export function HttpLoaderFactory(http: HttpClient) {
 	],
 	declarations: [
 		HeaderComponent,
-		ErrorMessageComponent
+		ErrorMessageComponent,
+		FooterComponent
 	],
 	providers: [
 		HttpClient
@@ -37,6 +39,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 	exports: [
 		TranslateModule,
 		HeaderComponent,
+		FooterComponent,
 		ErrorMessageComponent
 	]
 })
