d86e4b2d5db3bdb5ebb384ca1fc49bf84da864ee
George Spasov <spasov05203@gmail.com>
2017-10-11 11:49:47 +0300

    Getting the authentication status of the user

--- src/app/properties/list-property/list-property.component.ts
+++ src/app/properties/list-property/list-property.component.ts
@@ -1,15 +1,29 @@
+import { AuthenticationService } from './../../authentication/authentication.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ErrorsService } from './../../shared/errors/errors.service';
+import { ErrorsDecoratableComponent } from './../../shared/errors/errors.decoratable.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-property',
-  templateUrl: './list-property.component.html',
-  styleUrls: ['./list-property.component.scss']
+	selector: 'app-list-property',
+	templateUrl: './list-property.component.html',
+	styleUrls: ['./list-property.component.scss']
 })
-export class ListPropertyComponent implements OnInit {
+export class ListPropertyComponent extends ErrorsDecoratableComponent implements OnInit {
 
-  constructor() { }
+	public isUserAnonymous: boolean;
+	public hasLoaded: boolean;
 
-  ngOnInit() {
-  }
+	constructor(errorsService: ErrorsService,
+		translateService: TranslateService,
+		private authService: AuthenticationService) {
+		super(errorsService, translateService);
+	}
+
+	async ngOnInit() {
+		const result = await this.authService.isUserAnyonymous();
+		this.isUserAnonymous = result;
+		this.hasLoaded = true;
+	}
 
 }
