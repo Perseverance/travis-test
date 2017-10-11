8383b434954d13483fd6bc9004439774eb826a57
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2017-10-11 12:00:08 +0300

    Form first steps

--- src/app/properties/list-property/list-property.component.ts
+++ src/app/properties/list-property/list-property.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ErrorsService} from '../../shared/errors/errors.service';
+import {TranslateService} from '@ngx-translate/core';
+import {ErrorsDecoratableComponent} from '../../shared/errors/errors.decoratable.component';
 
 @Component({
-  selector: 'app-list-property',
-  templateUrl: './list-property.component.html',
-  styleUrls: ['./list-property.component.scss']
+	selector: 'app-list-property',
+	templateUrl: './list-property.component.html',
+	styleUrls: ['./list-property.component.scss']
 })
-export class ListPropertyComponent implements OnInit {
+export class ListPropertyComponent extends ErrorsDecoratableComponent implements OnInit {
+	public listPropertyForm: FormGroup;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+	constructor(private formBuilder: FormBuilder,
+				errorsService: ErrorsService,
+				translateService: TranslateService) {
+		super(errorsService, translateService);
+
+		this.listPropertyForm = this.formBuilder.group({
+			email: ['',
+				[Validators.required, Validators.email]
+			],
+			firstName: ['', [Validators.required]],
+			lastName: ['', [Validators.required]]
+		});
+	}
+
+	ngOnInit() {
+	}
 
 }
--- src/app/properties/properties.module.ts
+++ src/app/properties/properties.module.ts
@@ -1,19 +1,25 @@
-import { PropertiesService } from './properties.service';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {PropertiesService} from './properties.service';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { PropertiesRoutingModule } from './properties-routing.module';
-import { PropertyDetailsComponent } from './property-details/property-details.component';
-import { ListPropertyComponent } from './list-property/list-property.component';
+import {PropertiesRoutingModule} from './properties-routing.module';
+import {PropertyDetailsComponent} from './property-details/property-details.component';
+import {ListPropertyComponent} from './list-property/list-property.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		PropertiesRoutingModule
+		FormsModule,
+		ReactiveFormsModule,
+		PropertiesRoutingModule,
+		TranslateModule
 	],
 	declarations: [PropertyDetailsComponent, ListPropertyComponent],
 	providers: [
 		PropertiesService
 	]
 })
-export class PropertiesModule { }
+export class PropertiesModule {
+}
