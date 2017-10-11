c9472f1423a5113d6b590634478e75f3385a936f
George Spasov <spasov05203@gmail.com>
2017-10-11 16:34:33 +0300

    Added property types

--- src/app/properties/list-property/list-property.component.ts
+++ src/app/properties/list-property/list-property.component.ts
@@ -1,11 +1,12 @@
-import {NotificationsService} from './../../shared/notifications/notifications.service';
-import {AuthenticationService} from './../../authentication/authentication.service';
-import {TranslateService} from '@ngx-translate/core';
-import {ErrorsService} from './../../shared/errors/errors.service';
-import {ErrorsDecoratableComponent} from './../../shared/errors/errors.decoratable.component';
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {Message} from 'primeng/primeng';
+import { NotificationsService } from './../../shared/notifications/notifications.service';
+import { AuthenticationService } from './../../authentication/authentication.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ErrorsService } from './../../shared/errors/errors.service';
+import { ErrorsDecoratableComponent } from './../../shared/errors/errors.decoratable.component';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Message } from 'primeng/primeng';
+import { SelectItem } from 'primeng/components/common/selectitem';
 
 @Component({
 	selector: 'app-list-property',
@@ -16,19 +17,44 @@ export class ListPropertyComponent extends ErrorsDecoratableComponent implements
 	public isUserAnonymous: boolean;
 	public hasLoaded: boolean;
 	public listPropertyForm: FormGroup;
+	public propertyTypes: SelectItem[];
 
 	msgs: Message[];
 
 	uploadedFiles: any[] = [];
 
 	constructor(private formBuilder: FormBuilder,
-				private authService: AuthenticationService,
-				errorsService: ErrorsService,
-				translateService: TranslateService,
-				private notificationService: NotificationsService) {
+		private authService: AuthenticationService,
+		errorsService: ErrorsService,
+		translateService: TranslateService,
+		private notificationService: NotificationsService) {
 		super(errorsService, translateService);
 
-		this.listPropertyForm = this.formBuilder.group({});
+		this.propertyTypes = [];
+		this.propertyTypes.push({ label: 'Select Type', value: null });
+		this.propertyTypes.push({ label: 'SingleFamilyHome', value: 1 });
+		this.propertyTypes.push({ label: 'Apartment', value: 2 });
+		this.propertyTypes.push({ label: 'Townhouse', value: 3 });
+		this.propertyTypes.push({ label: 'Condo', value: 4 });
+		this.propertyTypes.push({ label: 'Coop', value: 5 });
+		this.propertyTypes.push({ label: 'Loft', value: 6 });
+		this.propertyTypes.push({ label: 'TIC', value: 7 });
+		this.propertyTypes.push({ label: 'Villa', value: 8 });
+		this.propertyTypes.push({ label: 'SummerVilla', value: 9 });
+		this.propertyTypes.push({ label: 'DevelopmentOnly', value: 10 });
+		this.propertyTypes.push({ label: 'Studio', value: 11 });
+		this.propertyTypes.push({ label: 'Maisonette', value: 12 });
+		this.propertyTypes.push({ label: 'Penthouse', value: 13 });
+		this.propertyTypes.push({ label: 'Bungalow', value: 14 });
+		this.propertyTypes.push({ label: 'StudentRoom', value: 15 });
+
+		this.listPropertyForm = this.formBuilder.group({
+			propertyType: ['', Validators.required]
+		});
+	}
+
+	public get propertyType() {
+		return this.listPropertyForm.get('propertyType');
 	}
 
 	async ngOnInit() {
@@ -62,5 +88,9 @@ export class ListPropertyComponent extends ErrorsDecoratableComponent implements
 			const base64data = reader.result;
 			console.log(base64data);
 		};
+	}
+
+	public onSubmit() {
+		console.log(this.propertyType.value);
 	}
 }
--- src/app/properties/properties.module.ts
+++ src/app/properties/properties.module.ts
@@ -1,14 +1,14 @@
-import {PropertiesService} from './properties.service';
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-
-import {PropertiesRoutingModule} from './properties-routing.module';
-import {PropertyDetailsComponent} from './property-details/property-details.component';
-import {ListPropertyComponent} from './list-property/list-property.component';
-import {TranslateModule} from '@ngx-translate/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {SharedModule} from '../shared/shared.module';
-import {FileUploadModule} from 'primeng/primeng';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PropertiesService } from './properties.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PropertiesRoutingModule } from './properties-routing.module';
+import { PropertyDetailsComponent } from './property-details/property-details.component';
+import { ListPropertyComponent } from './list-property/list-property.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { FileUploadModule, DropdownModule } from 'primeng/primeng';
 
 @NgModule({
 	imports: [
@@ -18,7 +18,9 @@ import {FileUploadModule} from 'primeng/primeng';
 		SharedModule,
 		FileUploadModule,
 		PropertiesRoutingModule,
-		TranslateModule
+		TranslateModule,
+		DropdownModule,
+		BrowserAnimationsModule
 	],
 	declarations: [PropertyDetailsComponent, ListPropertyComponent],
 	providers: [
