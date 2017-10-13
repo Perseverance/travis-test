f2c67bb7fa6a7d12d1d92be6174c4d45289769a6
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2017-10-13 14:51:27 +0300

    Add isProdEnv variable in deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,5 @@
 ::Deployment
-echo Handling swip frontend deployment.
+echo Handling Propy frontend deployment.
 
 :: 1. Install npm packages
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
@@ -7,7 +7,12 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   call npm install npm@latest -g
   call npm install --no-optional
   call npm cache clean --force
-  call .\node_modules\.bin\ng build
+  IF "%IsProdEnvironment%" (
+  	call .\node_modules\.bin\ng build --prod --aot
+  )
+  ELSE (
+  	call .\node_modules\.bin\ng build --aot
+  )
   IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
     call "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
     IF !ERRORLEVEL! NEQ 0 goto error
