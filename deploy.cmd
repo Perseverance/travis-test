67e4ea807828e226ed34ce9be39b8450da3cbb35
George Spasov <spasov05203@gmail.com>
2017-10-13 17:12:20 +0300

    Fixed deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -7,12 +7,8 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   call npm install npm@latest -g
   call npm install --no-optional
   call npm cache clean --force
-  IF %IsProdEnvironment% == true (
-  	call .\node_modules\.bin\ng build --prod --aot
-  )
-  ELSE (
-  	call .\node_modules\.bin\ng build --aot
-  )
+  call .\node_modules\.bin\ng build --prod --aot
+  
   IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
     call "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
     IF !ERRORLEVEL! NEQ 0 goto error
