2e15c8d2bf5d8ff9245a32806df2e1f77ad4c4d7
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2017-10-13 11:52:16 +0300

    Lazy loading map module and restructuring of shared/core modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,37 +1,14 @@
-import { NotificationsService } from './notifications/notifications.service';
-import { ErrorsService } from './errors/errors.service';
-import { AgmCoreModule } from '@agm/core';
-import { environment } from './../../environments/environment';
-import { SessionStorageService } from './session-storage.service';
-import { APIEndpointsService } from './apiendpoints.service';
-import { LocalStorageService } from './localStorage.service';
-import { RestClientService } from './rest-client.service';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LocationSearchComponent } from '../location-search/location-search.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { TokenGuard } from '../authentication/token-guard.service';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LocationSearchComponent} from '../location-search/location-search.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		ReactiveFormsModule,
-		AgmCoreModule.forRoot({
-			apiKey: environment.googleMaps.apiKey,
-			libraries: ['places'],
-			language: environment.googleMaps.defaultLanguage
-		})
+		ReactiveFormsModule
 	],
 	declarations: [LocationSearchComponent],
-	providers: [
-		RestClientService,
-		LocalStorageService,
-		SessionStorageService,
-		APIEndpointsService,
-		ErrorsService,
-		NotificationsService,
-		TokenGuard
-	],
 	exports: [LocationSearchComponent]
 })
 export class SharedModule {
