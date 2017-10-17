ab1f7e8ad2c9c6be2ac02680485923f39d1fcd76
George Spasov <g.spasov@propy.com>
2017-10-17 12:21:10 +0000

    Merged in smart-app-banner (pull request #85)
    Added Smart App Banner
    
    * Added Smart App Banner
    
    * Moved smart banner from header to the landing
    
    Approved-by: Vladislav Ivanov <v.ivanov@propy.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthenticationService} from '../authentication/authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication/authentication.service';
+
+declare function SmartBanner(param1: any): void;
 
 @Component({
 	selector: 'app-home',
@@ -10,14 +12,30 @@ import {AuthenticationService} from '../authentication/authentication.service';
 export class HomeComponent implements OnInit {
 
 	constructor(private router: Router,
-				private authService: AuthenticationService) {
+		private authService: AuthenticationService) {
 	}
 
 	ngOnInit() {
 		this.authService.refreshTokenOrLoginAnonym();
+		const banner = new SmartBanner({
+			// force: 'ios', // Uncomment for platform emulation
+			daysHidden: 7, // days to hide banner after close button is clicked (defaults to 15)
+			daysReminder: 30, // days to hide banner after "VIEW" button is clicked (defaults to 90)
+			appStoreLanguage: 'us', // language code for the App Store (defaults to user's browser language)
+			title: 'Propy',
+			author: 'Propy Inc',
+			button: 'View',
+			store: {
+				ios: 'In App Store'
+			},
+			price: {
+				ios: 'FREE'
+			},
+			icon: '/assets/images/propy-app-image.jpg' // full path to icon image if not using website icon image
+		});
 	}
 
 	onLocationFoundHandler(latitude: number, longitude: number) {
-		this.router.navigate(['map', {latitude: latitude, longitude: longitude}]);
+		this.router.navigate(['map', { latitude: latitude, longitude: longitude }]);
 	}
 }
