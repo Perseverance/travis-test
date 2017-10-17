61d0fbd34fa1a5986b1a49f2d0b093c9d5b7fa37
George Spasov <spasov05203@gmail.com>
2017-10-17 15:04:29 +0300

    Fixed year

--- src/app/properties/list-property/list-property.component.ts
+++ src/app/properties/list-property/list-property.component.ts
@@ -1,15 +1,15 @@
-import {CreatePropertyResponse, PropertyImage} from './../properties-responses';
-import {PropertiesService} from './../properties.service';
-import {NotificationsService} from './../../shared/notifications/notifications.service';
-import {AuthenticationService, UserData} from './../../authentication/authentication.service';
-import {TranslateService} from '@ngx-translate/core';
-import {ErrorsService} from './../../shared/errors/errors.service';
-import {ErrorsDecoratableComponent} from './../../shared/errors/errors.decoratable.component';
-import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {SelectItem} from 'primeng/components/common/selectitem';
-import {DefaultAsyncAPIErrorHandling} from '../../shared/errors/errors.decorators';
-import {LocationSearchComponent} from '../../location-search/location-search.component';
+import { CreatePropertyResponse, PropertyImage } from './../properties-responses';
+import { PropertiesService } from './../properties.service';
+import { NotificationsService } from './../../shared/notifications/notifications.service';
+import { AuthenticationService, UserData } from './../../authentication/authentication.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ErrorsService } from './../../shared/errors/errors.service';
+import { ErrorsDecoratableComponent } from './../../shared/errors/errors.decoratable.component';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SelectItem } from 'primeng/components/common/selectitem';
+import { DefaultAsyncAPIErrorHandling } from '../../shared/errors/errors.decorators';
+import { LocationSearchComponent } from '../../location-search/location-search.component';
 
 @Component({
 	selector: 'app-list-property',
@@ -31,60 +31,62 @@ export class ListPropertyComponent extends ErrorsDecoratableComponent implements
 	public hasUserLoaded = false;
 	public isUserAnonymous: boolean;
 
+	private DEFAULT_COMMISION_FEE = 75;
+
 	private SELECT_TYPE_KEY = 'list-property.form-fields.select-type';
 
 	@ViewChild(LocationSearchComponent)
 	private locationSearchComponent: LocationSearchComponent;
 
 	constructor(private formBuilder: FormBuilder,
-				private authService: AuthenticationService,
-				errorsService: ErrorsService,
-				translateService: TranslateService,
-				private notificationService: NotificationsService,
-				private propertiesService: PropertiesService) {
+		private authService: AuthenticationService,
+		errorsService: ErrorsService,
+		translateService: TranslateService,
+		private notificationService: NotificationsService,
+		private propertiesService: PropertiesService) {
 		super(errorsService, translateService);
 
 		this.propertyTypes = [];
-		this.propertyTypes.push({label: '', value: null});
-		this.propertyTypes.push({label: 'SingleFamilyHome', value: 1});
-		this.propertyTypes.push({label: 'Apartment', value: 2});
-		this.propertyTypes.push({label: 'Townhouse', value: 3});
-		this.propertyTypes.push({label: 'Condo', value: 4});
-		this.propertyTypes.push({label: 'Coop', value: 5});
-		this.propertyTypes.push({label: 'Loft', value: 6});
-		this.propertyTypes.push({label: 'TIC', value: 7});
-		this.propertyTypes.push({label: 'Villa', value: 8});
-		this.propertyTypes.push({label: 'SummerVilla', value: 9});
-		this.propertyTypes.push({label: 'DevelopmentOnly', value: 10});
-		this.propertyTypes.push({label: 'Studio', value: 11});
-		this.propertyTypes.push({label: 'Maisonette', value: 12});
-		this.propertyTypes.push({label: 'Penthouse', value: 13});
-		this.propertyTypes.push({label: 'Bungalow', value: 14});
-		this.propertyTypes.push({label: 'StudentRoom', value: 15});
+		this.propertyTypes.push({ label: '', value: null });
+		this.propertyTypes.push({ label: 'SingleFamilyHome', value: 1 });
+		this.propertyTypes.push({ label: 'Apartment', value: 2 });
+		this.propertyTypes.push({ label: 'Townhouse', value: 3 });
+		this.propertyTypes.push({ label: 'Condo', value: 4 });
+		this.propertyTypes.push({ label: 'Coop', value: 5 });
+		this.propertyTypes.push({ label: 'Loft', value: 6 });
+		this.propertyTypes.push({ label: 'TIC', value: 7 });
+		this.propertyTypes.push({ label: 'Villa', value: 8 });
+		this.propertyTypes.push({ label: 'SummerVilla', value: 9 });
+		this.propertyTypes.push({ label: 'DevelopmentOnly', value: 10 });
+		this.propertyTypes.push({ label: 'Studio', value: 11 });
+		this.propertyTypes.push({ label: 'Maisonette', value: 12 });
+		this.propertyTypes.push({ label: 'Penthouse', value: 13 });
+		this.propertyTypes.push({ label: 'Bungalow', value: 14 });
+		this.propertyTypes.push({ label: 'StudentRoom', value: 15 });
 
 		this.currencies = [];
-		this.currencies.push({label: 'USD', value: 1});
-		this.currencies.push({label: 'EUR', value: 2});
-		this.currencies.push({label: 'RUB', value: 3});
-		this.currencies.push({label: 'AED', value: 4});
-		this.currencies.push({label: 'HKD', value: 5});
-		this.currencies.push({label: 'SGD', value: 6});
-		this.currencies.push({label: 'GBP', value: 7});
-		this.currencies.push({label: 'BGN', value: 8});
-		this.currencies.push({label: 'CNY', value: 9});
-		this.currencies.push({label: 'ETH', value: 10});
-		this.currencies.push({label: 'BTC', value: 11});
+		this.currencies.push({ label: 'USD', value: 1 });
+		this.currencies.push({ label: 'EUR', value: 2 });
+		this.currencies.push({ label: 'RUB', value: 3 });
+		this.currencies.push({ label: 'AED', value: 4 });
+		this.currencies.push({ label: 'HKD', value: 5 });
+		this.currencies.push({ label: 'SGD', value: 6 });
+		this.currencies.push({ label: 'GBP', value: 7 });
+		this.currencies.push({ label: 'BGN', value: 8 });
+		this.currencies.push({ label: 'CNY', value: 9 });
+		this.currencies.push({ label: 'ETH', value: 10 });
+		this.currencies.push({ label: 'BTC', value: 11 });
 
 		this.areaUnits = [];
-		this.areaUnits.push({label: 'sqm', value: 1});
-		this.areaUnits.push({label: 'sqft', value: 2});
+		this.areaUnits.push({ label: 'sqm', value: 1 });
+		this.areaUnits.push({ label: 'sqft', value: 2 });
 
 		this.listPropertyForm = this.formBuilder.group({
 			propertyType: ['', Validators.required],
 			furnished: [''],
 			price: ['', Validators.required],
 			acceptedCurrencies: [[], Validators.required],
-			feeSlider: [75],
+			commisionFee: [this.DEFAULT_COMMISION_FEE],
 			currency: ['1', Validators.required],
 			year: [''],
 			bedrooms: [''],
@@ -134,16 +136,21 @@ export class ListPropertyComponent extends ErrorsDecoratableComponent implements
 		return this.listPropertyForm.get('acceptedCurrencies');
 	}
 
-	public get feeSlider() {
-		return this.listPropertyForm.get('feeSlider');
+	public get commisionFee() {
+		return this.listPropertyForm.get('commisionFee');
 	}
 
 	public get feeDecimal() {
-		return this.feeSlider.value / 10;
+		return this.commisionFee.value / 10;
 	}
 
 	public get year() {
 		return this.listPropertyForm.get('year');
+	}
+
+	public get yearInListingFormat() {
+		const yearBuilt = this.year.value;
+		return `01-01-${yearBuilt}`;
 	}
 
 	public get bedrooms() {
@@ -301,6 +308,7 @@ export class ListPropertyComponent extends ErrorsDecoratableComponent implements
 				value: this.area.value,
 				type: this.areaUnit.value
 			},
+			builtIn: this.yearInListingFormat,
 			type: this.propertyType.value,
 			description: this.description.value,
 			address: this.address.value,
@@ -339,6 +347,7 @@ export class ListPropertyComponent extends ErrorsDecoratableComponent implements
 		this.listPropertyForm.reset();
 		this.currency.setValue('1'); // USD default
 		this.areaUnit.setValue('1'); // sqm default
+		this.commisionFee.setValue(this.DEFAULT_COMMISION_FEE)
 		this.locationSearchComponent.resetInput();
 	}
 }
