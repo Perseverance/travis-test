4272d41c6d2bd4e3ef0b72511a198c2bd78574bc
Georgi Spasov <george@limelabs.tech>
2017-11-02 18:10:52 +0200

    Minor fixes on the list property and image urls

--- src/app/shared/pipes/image-environment-prefix.pipe.ts
+++ src/app/shared/pipes/image-environment-prefix.pipe.ts
@@ -1,12 +1,15 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {environment} from '../../../environments/environment';
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
-@Pipe({name: 'imageEnvPrefix'})
+@Pipe({ name: 'imageEnvPrefix' })
 export class ImageEnvironmentPrefixPipe implements PipeTransform {
 
 	transform(value: string): string {
+		if (value.startsWith('http')) {
+			return value;
+		}
 		let imageUrl: string;
-		imageUrl = `${environment.apiUrl}${value}`
+		imageUrl = `${environment.apiUrl}${value}`;
 		return imageUrl;
 	}
 }
