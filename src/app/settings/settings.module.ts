dd72c63b314b702e4a30075a2c9cc969cc820271
Georgi Spasov <george@limelabs.tech>
2017-11-09 18:21:00 +0200

    Settings routing and navigation

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings/settings.component';
+import { TabViewModule } from 'primeng/components/tabview/tabview';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SettingsRoutingModule
-  ],
-  declarations: [SettingsComponent]
+	imports: [
+		CommonModule,
+		SettingsRoutingModule,
+		TabViewModule
+	],
+	declarations: [SettingsComponent]
 })
 export class SettingsModule { }
