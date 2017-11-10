7df89ad32d22dd3c72fcccf4c841e3fa2c33b50e
Georgi Spasov <george@limelabs.tech>
2017-11-10 11:10:18 +0200

    Added general-settings component

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,16 +1,19 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings/settings.component';
 import { TabViewModule } from 'primeng/components/tabview/tabview';
+import { GeneralSettingsComponent } from './general-settings/general-settings.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		SettingsRoutingModule,
-		TabViewModule
+		TabViewModule,
+		TranslateModule
 	],
-	declarations: [SettingsComponent]
+	declarations: [SettingsComponent, GeneralSettingsComponent]
 })
 export class SettingsModule { }
