3c96e8c488526f3eb78b174a983f90520a30264e
Ivan Ivanov <i.ivanov@propy.com>
2017-11-14 09:32:47 +0200

    newsletter component added

---
+++ src/app/home/newsletter/newsletter.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {Jsonp} from '@angular/http';
+
+@Component({
+	selector: 'app-newsletter',
+	templateUrl: './newsletter.component.html',
+	styleUrls: ['./newsletter.component.scss']
+})
+export class NewsletterComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit() {
+	}
+}
