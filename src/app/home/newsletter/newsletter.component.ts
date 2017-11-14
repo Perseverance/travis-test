907dc298834e82b87fbb2f4e17e51d3fc9d10f59
Ivan Ivanov <i.ivanov@propy.com>
2017-11-14 16:28:34 +0200

    newsletter section finished

--- src/app/home/newsletter/newsletter.component.ts
+++ src/app/home/newsletter/newsletter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Jsonp} from '@angular/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
 	selector: 'app-newsletter',
@@ -8,7 +9,11 @@ import {Jsonp} from '@angular/http';
 })
 export class NewsletterComponent implements OnInit {
 
-	constructor() {
+	public sendMailChimpForm: FormGroup;
+	constructor(private formBuilder: FormBuilder) {
+		this.sendMailChimpForm = this.formBuilder.group({
+			email: ['', []]
+		});
 	}
 
 	ngOnInit() {
