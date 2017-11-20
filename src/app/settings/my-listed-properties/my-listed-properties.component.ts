0e07bf036dad9e67f83eae0f0330228c53ee4f04
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2017-11-20 14:41:52 +0200

    Confirmation styled

--- src/app/settings/my-listed-properties/my-listed-properties.component.ts
+++ src/app/settings/my-listed-properties/my-listed-properties.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {PropertiesService} from '../../properties/properties.service';
 import {ConfirmationService, Message} from 'primeng/primeng';
+import {PropertyStatusEnum} from '../../shared/enums/property-status.enum';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-my-listed-properties',
@@ -10,29 +12,45 @@ import {ConfirmationService, Message} from 'primeng/primeng';
 export class MyListedPropertiesComponent implements OnInit {
 	public myListedProperties;
 	public messages: Message[] = [];
+	public confirmationLabels: object;
 
 	constructor(private propertiesService: PropertiesService,
-				private confirmationService: ConfirmationService) {
+				private confirmationService: ConfirmationService,
+				private translateService: TranslateService) {
 	}
 
 	async ngOnInit() {
 		this.myListedProperties = await this.propertiesService.getMyListedProperties();
-		console.log(this.myListedProperties);
+
+		this.translateService.stream([
+			'settings.my-listed-properties-settings.confirmation-heading',
+			'settings.my-listed-properties-settings.confirmation-message'
+		]).subscribe((translations) => {
+			this.confirmationLabels = {
+				heading: translations['settings.my-listed-properties-settings.confirmation-heading'],
+				message: translations['settings.my-listed-properties-settings.confirmation-message']
+			};
+		});
 	}
 
 	public markPropertyAsSold(id: string) {
 		this.confirmationService.confirm({
-			message: 'Are you sure that you want to proceed?',
-			header: 'Confirmation',
-			icon: 'fa fa-question-circle',
+			message: this.confirmationLabels['message'],
+			header: this.confirmationLabels['heading'],
 			accept: async () => {
 				const result = await this.propertiesService.markPropertyAsSold(id);
 				if (result) {
+					for (let i = 0; i < this.myListedProperties.length; i++) {
+						const property = this.myListedProperties[i];
+						if (property.id === id) {
+							this.myListedProperties[i].status = PropertyStatusEnum.Sold;
+							break;
+						}
+					}
 					this.messages = [{severity: 'info', summary: 'Confirmed', detail: 'You have accepted'}];
 				}
 			},
 			reject: () => {
-				this.messages = [{severity: 'info', summary: 'Rejected', detail: 'You have rejected'}];
 			}
 		});
 	}
