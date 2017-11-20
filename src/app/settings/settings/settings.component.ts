c02407fd5ce5e25b91199055a813dfe008a2865c
George Spasov <g.spasov@propy.com>
2017-11-20 15:37:17 +0000

    Merged in metamask (pull request #184)
    web3 added
    
    Approved-by: Vladislav Ivanov <v.ivanov@propy.com>

--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {AuthenticationService, UserData} from './../../authentication/authentication.service';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AuthenticationService, UserData } from './../../authentication/authentication.service';
+import { Web3Service } from '../../web3/web3.service';
 
 @Component({
 	selector: 'app-settings',
@@ -10,7 +11,8 @@ import {AuthenticationService, UserData} from './../../authentication/authentica
 export class SettingsComponent implements OnInit {
 	public shouldShowPassword: any;
 
-	constructor(private authService: AuthenticationService) {
+	constructor(private authService: AuthenticationService,
+		private web3Service: Web3Service) {
 	}
 
 	ngOnInit() {
@@ -24,6 +26,23 @@ export class SettingsComponent implements OnInit {
 					this.shouldShowPassword = userInfo.user.externalLoginProviders.length === 0;
 				}
 			}
+		});
+		this.web3Service.web3InstanceLoaded({
+			next: (web3Loaded: boolean) => {
+				this.getMetamaskAccounts();
+			}
+		});
+	}
+
+	private getMetamaskAccounts() {
+		this.web3Service.getMetmaskAccounts().subscribe({
+			error: (err) => {
+				console.error(err);
+			},
+			next: (accs) => {
+				console.log(accs);
+			},
+			complete: () => { }
 		});
 	}
 
