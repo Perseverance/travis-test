259540d0fd08f56f14160a73d10a8c17c6918f63
George Spasov <g.spasov@propy.com>
2017-12-01 09:28:52 +0000

    Merged in smart-contract-connection-mock (pull request #213)
    Smart contract connection mock
    
    * Settlement statement & Closing document
    
    * Methods for payment
    
    Approved-by: Vladislav Ivanov <v.ivanov@propy.com>

--- src/app/smart-contract-connection/smart-contract-connection.service.ts
+++ src/app/smart-contract-connection/smart-contract-connection.service.ts
@@ -7,6 +7,8 @@ export enum Status {
 	verifed,
 	purchaseAgreement,
 	settlementStatement,
+	sellerDisclosures,
+	closingDocuments,
 	payment,
 	receivedPayment,
 	titleDeed,
@@ -73,7 +75,8 @@ export class SmartContractConnectionService {
 		propertyLocationAddress: string,
 		sellerAddress: EthereumAddress,
 		brokerAddress: EthereumAddress,
-		escrowAddress: EthereumAddress): Promise<SmartContractAddress> {
+		escrowAddress: EthereumAddress,
+		price: number): Promise<SmartContractAddress> {
 		return this.fakeDeedAddress;
 	}
 
@@ -137,5 +140,28 @@ export class SmartContractConnectionService {
 		return true;
 	}
 
+	public async markClosingDocumentsUploaded(closingDocumentsSignatureRequestId: string): Promise<boolean> {
+		return true;
+	}
+
+	public async getClosingDocumentsSignatureRequestId(deedContractAddress: string): Promise<string> {
+		return 'hardcoded_request_id';
+	}
+
+	public async signClosingDocuments(deedContractAddress: string, closingDocumentsSignatureRequestId: string): Promise<boolean> {
+		return true;
+	}
+
+	public async getDeedPrice(deedContractAddress: string): Promise<number> {
+		return 200000;
+	}
+
+	public async sendPayment(deedContractAddress: string): Promise<boolean> {
+		return true;
+	}
+
+	public async markPaymentReceived(deedContractAddress: string): Promise<boolean> {
+		return true;
+	}
 
 }
