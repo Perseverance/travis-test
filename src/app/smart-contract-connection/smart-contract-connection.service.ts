88db1c1068d7a28916640c4c36abe0666cd21395
Georgi Spasov <george@limelabs.tech>
2017-12-07 15:00:29 +0200

    Extended sign document component

--- src/app/smart-contract-connection/smart-contract-connection.service.ts
+++ src/app/smart-contract-connection/smart-contract-connection.service.ts
@@ -31,7 +31,7 @@ export class SmartContractConnectionService {
 	constructor() {
 	}
 
-	private fakeDeedStatus = Status.purchaseAgreement;
+	private fakeDeedStatus = Status.settlementStatement;
 
 	private fakeDeedAddress = '0x406e4e45785acf237c05c8f0d80dd2b11e4042db';
 	private fakePropertyAddress = 'Kmetska Sgrada, Gabrovo, Gabrovo, Bulgaria';
