a12084b3ed8ee5c03219b82b1e7838854dc3031d
Ognyan Chikov <ognyanchikov@Ognyans-MacBook-Pro.local>
2017-12-08 11:33:39 +0200

    fix another comments

--- src/app/shared/pipes/text-limitation.pipe.ts
+++ src/app/shared/pipes/text-limitation.pipe.ts
@@ -1,17 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
-const defaultValue = 250;
+
 @Pipe({
   name: 'textLimitation'
 })
 
 export class TextLimitationPipe implements PipeTransform {
-
+  private DEFAULT_TEXT_LIMIT = 250;
   transform(value: string, limit?: number) {
     if (!value) {
       throw new Error('No value for pipe supplied');
     }
-    const actualLimit = (limit) ? limit : defaultValue;
-    if (value.length < actualLimit) {
+    const actualLimit = (limit) ? limit : this.DEFAULT_TEXT_LIMIT;
+    if (value.length <= actualLimit) {
       return value;
     }
     return value.substr(0, actualLimit) + '...';
