e0ccf9b904ebea94d13518761623eacbeca9a90f
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2017-12-13 14:41:31 +0200

    Change settlement statement to title report

--- src/app/transaction-tool/title-report/title-report.component.spec.ts
+++ src/app/transaction-tool/title-report/title-report.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SettlementStatementStepComponent } from './settlement-statement-step.component';
+import { SettlementStatementStepComponent } from './title-report-step.component';
 
 describe('SettlementStatementStepComponent', () => {
   let component: SettlementStatementStepComponent;
--- src/app/transaction-tool/workflow/workflow.model.ts
+++ src/app/transaction-tool/workflow/workflow.model.ts
@@ -2,7 +2,7 @@ export const STEPS = {
 	'property-preview': 0,
 	'invite': 1,
 	'purchase-agreement': 2,
-	'settlement-statement': 3,
+	'title-report': 3,
 	'seller-disclosures': 4,
 	'closing-documents': 5,
 	'payment': 6
@@ -12,7 +12,7 @@ export const REVERSE_STEPS = {
 	0: 'property-preview',
 	1: 'invite',
 	2: 'purchase-agreement',
-	3: 'settlement-statement',
+	3: 'title-report',
 	4: 'seller-disclosures',
 	5: 'closing-documents',
 	6: 'payment'
