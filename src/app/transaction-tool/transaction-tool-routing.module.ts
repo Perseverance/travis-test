11bb9befd2da325f93d74243ae5db4e9a9615ae7
Ognyan Chikov <o.chikov@propy.com>
2017-12-14 14:41:07 +0000

    Merged in payment-design (pull request #255)
    Payment design
    
    * implement the design of payment step
    
    * fix the design of the price
    
    * fix routes to be as they were
    
    * remove comments from code
    
    * implement copy button next to the address of payment
    
    * fix issues
    
    * fix the comments from the Daily
    
    * fix the onSubmit method
    
    * final fixes in route module and class names
    
    Approved-by: Vladislav Ivanov <v.ivanov@propy.com>

--- src/app/transaction-tool/transaction-tool-routing.module.ts
+++ src/app/transaction-tool/transaction-tool-routing.module.ts
@@ -1,17 +1,17 @@
-import {InviteComponent} from './invite/invite.component';
-import {PaymentStepComponent} from './payment-step/payment-step.component';
-import {TitleReportComponent} from './title-report/title-report.component';
-import {SellerDisclosuresStepComponent} from './seller-disclosures-step/seller-disclosures-step.component';
-import {PropertyPreviewComponent} from './property-preview/property-preview.component';
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
-import {TransactionToolComponent} from './transaction-tool.component';
-import {PurchaseAgreementStepComponent} from './purchase-agreement-step/purchase-agreement-step.component';
-import {WorkflowGuard} from './workflow/workflow-guard.service';
-import {TokenGuard} from '../authentication/token-guard.service';
-import {AuthenticatedGuard} from '../authentication/authenticated-guard.service';
-import {SettlementStatementComponent} from './settlement-statement/settlement-statement.component';
+import { InviteComponent } from './invite/invite.component';
+import { PaymentStepComponent } from './payment-step/payment-step.component';
+import { TitleReportComponent } from './title-report/title-report.component';
+import { SellerDisclosuresStepComponent } from './seller-disclosures-step/seller-disclosures-step.component';
+import { PropertyPreviewComponent } from './property-preview/property-preview.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { TransactionToolComponent } from './transaction-tool.component';
+import { PurchaseAgreementStepComponent } from './purchase-agreement-step/purchase-agreement-step.component';
+import { WorkflowGuard } from './workflow/workflow-guard.service';
+import { TokenGuard } from '../authentication/token-guard.service';
+import { AuthenticatedGuard } from '../authentication/authenticated-guard.service';
+import { SettlementStatementComponent } from './settlement-statement/settlement-statement.component';
 
 const transactionToolSteps: Routes = [
 	{
