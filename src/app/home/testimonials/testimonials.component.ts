148d53b75067a58cd1cbd5e83be077b23cdeb483
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2018-01-05 13:40:50 +0200

    Baum removal

--- src/app/home/testimonials/testimonials.component.ts
+++ src/app/home/testimonials/testimonials.component.ts
@@ -11,8 +11,8 @@ import {TranslateService} from '@ngx-translate/core';
 export class TestimonialsComponent implements OnInit {
 	public testemonialsDescriptions = [];
 	public carouselTile: NgxCarousel;
+	private PINEL_WORDS_KEY = 'landing.testimonials.pinel-words';
 	private ARRINGTON_WORDS_KEY = 'landing.testimonials.arrington-words';
-	private BAUM_WORDS_KEY = 'landing.testimonials.baum-words';
 	private GARDNER_WORDS_KEY = 'landing.testimonials.gardner-words';
 
 	constructor(public translateService: TranslateService) {
@@ -40,7 +40,7 @@ export class TestimonialsComponent implements OnInit {
 			easing: 'ease'
 		};
 
-		this.translateService.stream(this.ARRINGTON_WORDS_KEY).subscribe((keyTranslation: string) => {
+		this.translateService.stream(this.PINEL_WORDS_KEY).subscribe((keyTranslation: string) => {
 			this.testemonialsDescriptions[0].value = keyTranslation;
 			this.testemonialsDescriptions[0].dummyValue = keyTranslation;
 			this.testemonialsDescriptions[1].dummyValue = keyTranslation;
@@ -48,10 +48,6 @@ export class TestimonialsComponent implements OnInit {
 		});
 
 		this.translateService.stream(this.ARRINGTON_WORDS_KEY).subscribe((keyTranslation: string) => {
-			this.testemonialsDescriptions[0].value = keyTranslation;
-		});
-
-		this.translateService.stream(this.BAUM_WORDS_KEY).subscribe((keyTranslation: string) => {
 			this.testemonialsDescriptions[1].value = keyTranslation;
 		});
 
