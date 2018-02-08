6dc37398aae1201ebd9cfc96dd69b0572f515c01
Ognyan Chikov <o.chikov@propy.com>
2018-02-08 17:50:33 +0200

    make featured properties touch enabled

--- src/app/home/featured-properties/featured-properties.component.ts
+++ src/app/home/featured-properties/featured-properties.component.ts
@@ -1,8 +1,8 @@
-import {Router} from '@angular/router';
-import {PropertiesService} from './../../properties/properties.service';
-import {NgxCarousel} from 'ngx-carousel';
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {AuthenticationService, UserData} from '../../authentication/authentication.service';
+import { Router } from '@angular/router';
+import { PropertiesService } from './../../properties/properties.service';
+import { NgxCarousel } from 'ngx-carousel';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AuthenticationService, UserData } from '../../authentication/authentication.service';
 
 @Component({
 	selector: 'app-featured-properties',
@@ -20,8 +20,8 @@ export class FeaturedPropertiesComponent implements OnInit {
 	public userInfo: any;
 
 	constructor(private propertiesService: PropertiesService,
-				private router: Router,
-				private authService: AuthenticationService) {
+		private router: Router,
+		private authService: AuthenticationService) {
 		this.authService.subscribeToUserData({
 			next: (userInfo: UserData) => {
 				this.userInfo = userInfo;
@@ -31,7 +31,7 @@ export class FeaturedPropertiesComponent implements OnInit {
 
 	async ngOnInit() {
 		this.carouselTile = {
-			grid: {xs: 1, sm: 2, md: 3, lg: 3, all: 0},
+			grid: { xs: 1, sm: 2, md: 3, lg: 3, all: 0 },
 			speed: 1000,
 			slide: 1,
 			interval: 4000,
@@ -39,6 +39,7 @@ export class FeaturedPropertiesComponent implements OnInit {
 				visible: true
 			},
 			easing: 'ease',
+			touch: true,
 			loop: true
 		};
 		try {
