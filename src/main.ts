5c47bd320f7de438d4cd157e47f4dfb1f4687901
NikolayWhither <nikolai.n.gospodinov@gmail.com>
2018-02-16 15:52:40 +0200

    add conditional loading of maps script

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,26 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 
+const mapsScript = document.createElement('script');
+mapsScript.setAttribute('type', 'text/javascript');
+mapsScript.setAttribute('async', '');
+mapsScript.setAttribute('defer', '');
+
+// Check if in China
+if (environment.china) {
+	mapsScript.setAttribute(
+		'src',
+		'https://maps.google.cn/maps/api/js?key=AIzaSyDzuZpv8iXJL2lPLqvKU0J4KdFONwSPjGk&libraries=places&callback'
+	);
+} else {
+	mapsScript.setAttribute(
+		'src',
+		'https://maps.googleapis.com/maps/api/js?key=AIzaSyDzuZpv8iXJL2lPLqvKU0J4KdFONwSPjGk&libraries=places&callback'
+	);
+}
+
+document.head.appendChild(mapsScript);
+
 if (environment.production) {
 	// HubSpot Embed Code appends to head - prod environment only
 	const scriptElement = document.createElement('script');
@@ -12,7 +32,9 @@ if (environment.production) {
 	scriptElement.setAttribute('async', '');
 	scriptElement.setAttribute('defer', '');
 	scriptElement.setAttribute('src', '//js.hs-scripts.com/4204561.js');
+
 	document.head.appendChild(scriptElement);
+
 
 	enableProdMode();
 }
