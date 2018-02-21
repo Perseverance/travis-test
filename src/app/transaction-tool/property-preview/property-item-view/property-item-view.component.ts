d44384f82f5c08e04b8ccfc1554a22a64fc7a403
Ognyan Chikov <o.chikov@propy.com>
2018-02-21 13:55:10 +0200

    change the property preview because when navigate to another notification with another deed the input property is not updated and the property preview component does not show the right property

--- src/app/transaction-tool/property-preview/property-item-view/property-item-view.component.ts
+++ src/app/transaction-tool/property-preview/property-item-view/property-item-view.component.ts
@@ -1,26 +1,39 @@
 import { DeedsService } from './../../../shared/deeds.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, SimpleChange } from '@angular/core';
 import { LoadPropertyService } from '../../load-property.service';
 import { PropertiesService } from '../../../properties/properties.service';
 import { CurrencyTypeEnum } from '../../../shared/enums/currency-type.enum';
+import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
 	selector: 'app-property-item-view',
 	templateUrl: './property-item-view.component.html',
 	styleUrls: ['./property-item-view.component.scss']
 })
-export class PropertyItemViewComponent implements OnInit {
+export class PropertyItemViewComponent implements OnInit, OnChanges {
 	public property: any;
 	public deed: any;
 	@Input() deedId;
+	private _deedId;
 	constructor(private propertiesService: PropertiesService,
 		private deedsService: DeedsService) { }
 
 	async ngOnInit() {
+		this.loadComponentData(this.deedId);
+	}
+
+	ngOnChanges(changes: SimpleChanges) {
+		const deedId: SimpleChange = changes.deedId;
+		this._deedId = deedId.currentValue;
+		console.log(this._deedId);
+		this.loadComponentData(this._deedId);
+
+	}
+
+	public async loadComponentData(deedId) {
 		const deed = await this.deedsService.getDeedDetails(this.deedId);
 		this.deed = deed;
 		const result = await this.propertiesService.getProperty(deed.propertyId, CurrencyTypeEnum.ETH);
 		this.property = result;
 	}
-
 }
