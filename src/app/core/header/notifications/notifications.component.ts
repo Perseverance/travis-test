2c77d1600b16ecbe3ab46f609828d60753cfbbe4
Ognyan Chikov <o.chikov@propy.com>
2018-02-21 16:53:29 +0200

    fix styles and add some comments, also fix

--- src/app/core/header/notifications/notifications.component.ts
+++ src/app/core/header/notifications/notifications.component.ts
@@ -12,6 +12,7 @@ import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 })
 export class NotificationsComponent implements OnInit {
 	private notificationSubscription: Subscription;
+	public iconClass;
 	@Output() onNewNotifications = new EventEmitter();
 	@Input() notifications: any[];
 	@Input() newNotifications: number;
@@ -22,10 +23,12 @@ export class NotificationsComponent implements OnInit {
 	ngOnInit() {
 		this.notificationSubscription = this.pusherService.subscribeToNotificationsSubject({
 			next: (data: any) => {
-				this.notifications.push(this.notificationMessageService.deSerializeData(data.message));
-				this.newNotifications++;
-				this.onNewNotifications.emit(this.newNotifications);
-				console.log(this.notificationMessageService.deSerializeData(data.message));
+				// TO DO when backend is fixed and the pusher send only one notification!
+				// this.notifications.push(this.notificationMessageService.deSerializeData(data.message));
+				// this.newNotifications++;
+				// this.onNewNotifications.emit(this.newNotifications);
+				//console.log(this.notificationMessageService.deSerializeData(data.message));
+				//console.log(data);
 			}
 		});
 		this.onNewNotifications.emit(this.newNotifications);
@@ -35,7 +38,8 @@ export class NotificationsComponent implements OnInit {
 	}
 
 	private async goToRelatedEvent(notification) {
-		switch (this.notificationMessageService.returnNotificationType(notification.deedInfo.deedStatus)) {
+		switch (this.notificationMessageService.returnNotificationType(notification.details.deedStatus)) {
+			// TO DO add another case type here when we implement new notification types
 			case 'reserved':
 			case 'invited':
 			case 'accepted':
@@ -46,9 +50,10 @@ export class NotificationsComponent implements OnInit {
 			case 'action':
 			case 'payment':
 			case 'transfer':
+				this.iconClass = 'fa fa-building-o';
 				notification.isSeen = true;
-				this.router.navigate(['transaction-tool', notification.deedInfo.deedId,
-					this.notificationMessageService.returnDeedStatus(notification.deedInfo.deedStatus)]);
+				this.router.navigate(['transaction-tool', notification.details.deedId,
+					this.notificationMessageService.returnDeedStatus(notification.details.deedStatus)]);
 				await this.notificationMessageService.setNotificaitonSeen(notification.notificationId);
 				break;
 		}
