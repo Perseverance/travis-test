4d9a514047084df019da56311bb0fbd19cb2d2c5
George Spasov <spasov05203@gmail.com>
2018-03-01 21:13:01 +0200

    Even more sensible build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if [[ "$TRAVIS_BRANCH" =~ ^sprint-.*$ ]]; then
 	docker build -f ./Dockerfiles/dev.dockerfile -t dev_website_image .
+	docker build -f ./Dockerfiles/test.dockerfile -t test_website_image .
 elif [[ "$TRAVIS_BRANCH" =~ ^stable-.*$ ]]; then
 	docker build -f ./Dockerfiles/stage.dockerfile -t stage_website_image .
+	docker build -f ./Dockerfiles/prod.dockerfile -t prod_website_image .
+elif [[ "$TRAVIS_BRANCH" =~ ^china-.*$ ]]; then
+	docker build -f ./Dockerfiles/china.dockerfile -t china_website_image .
 fi
