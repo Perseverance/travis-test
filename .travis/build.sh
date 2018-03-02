ab649a8b3ee3ca8194dbf9c1711af0b1c1bd62b1
George Spasov <spasov05203@gmail.com>
2018-03-02 11:17:33 +0200

    Fixed bug in the build

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,7 +2,7 @@
 if [[ "$TRAVIS_BRANCH" =~ ^sprint-.*$ ]]; then
 	docker build -f ./Dockerfiles/dev.dockerfile -t dev_website_image .
 	docker build -f ./Dockerfiles/test.dockerfile -t test_website_image .
-elif [[ "$TRAVIS_BRANCH" =~ ^stable-.*$ ]]; then
+elif [[ "$TRAVIS_BRANCH" =~ ^stable.*$ ]]; then
 	docker build -f ./Dockerfiles/stage.dockerfile -t stage_website_image .
 	docker build -f ./Dockerfiles/prod.dockerfile -t propy/webmain .
 	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
